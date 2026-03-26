de5251fd97ddda21f77a3547b9ab3f9aa7b71c26
沐白 <mubai@mubaideMacBook-Air.local>
2026-03-26 13:54:31 +0800

    refactor: 简洁科技风重写 - 纯CSS Grid布局

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
 import Home from './pages/Home'
 import Posts from './pages/Posts'
 import PostDetail from './pages/PostDetail'
 import Category from './pages/Category'
 import About from './pages/About'
-import Privacy from './pages/Privacy'
-import Terms from './pages/Terms'
+import './index.css'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/posts/:slug" element={<PostDetail />} />
-          <Route path="/category/:category" element={<Category />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<Terms />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:slug" element={<PostDetail />} />
+        <Route path="/category/:category" element={<Category />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   )
 }
